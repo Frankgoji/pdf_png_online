219799bc0916f51c6ba1f4b8679254f8d1e30b77
Franklin Lee <frankgoji@gmail.com>
2017-01-19 02:45:03 -0800

    some framework done

--- pdf_convert.py
+++ pdf_convert.py
@@ -1,5 +1,11 @@
+#! /usr/bin/python3
 ################################################################################
 # Convert PDFs: Download, convert to PNG, serve, and delete to save space      #
 ################################################################################
 
-import cgi, cgitb
+import cgi, cgitb, sys
+cgitb.enable()
+
+with open("GodzillaTurtleducks.jpg", 'rb') as gtd:
+    sys.stdout.buffer.write(b"Content-Type: image/png\n\n")
+    sys.stdout.buffer.write(gtd.read())
